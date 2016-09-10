380de1b16f7db5783ca131e36b8887698e465962
Laury Lafage <lowrey97x@hotmail.fr>
2016-09-10 11:42:18 +0200

    Move watson code

--- robot/stt/watson/watson.py
+++ robot/stt/watson/watson.py
@@ -2,8 +2,13 @@ from ...robot import Robot
 
 from stt_watson.SttWatson import SttWatson
 from stt_watson.SttWatsonLogListener import SttWatsonLogListener
-from config.Config import Config
-import os.path
+from recording.Record import Record
+from watson_client.Client import Client
+from utils.SignalHandler import SignalHandler
+
+import threading
+import signal
+import os
 import argparse
 import pkgutil
 import yaml
@@ -11,12 +16,30 @@ import yaml
 class WatsonRobot(Robot):
     def __init__(self, config, speaker, actions):
         super(WatsonRobot, self).__init__(config, speaker, actions)
+        config['audio-chunk'] = 8000
+        config['audio-rate'] = 44100
+        config['channels'] = 1
+
+        self.listeners = []
+        sttWatsonLogListener = SttWatsonLogListener()
+        self.listeners.append(sttWatsonLogListener)
+        self.stopper = threading.Event()
+        self.record = Record(config, self.stopper)
+        self.workers = [self.record]
+        self.watsonClient = Client(config)
+        self.handler = SignalHandler(self.stopper, self.workers)
+        signal.signal(signal.SIGINT, self.handler)
 
     def name(self):
         return 'Watson'
 
     def listen(self):
-        sttWatsonLogListener = SttWatsonLogListener()
+        audioFd, writer = os.pipe()
+        self.record.setWriter(writer)
+        self.record.start()
+        self.watsonClient.setListeners(self.listeners)
+        self.watsonClient.startStt(audioFd)
+
         sttWatson = SttWatson(self.config)
         sttWatson.addListener(sttWatsonLogListener)
         sttWatson.run()
